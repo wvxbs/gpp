4fe61105baa5bfffb6cf374894383e59040b131b
Gabriel  Ferreira <gabriel.ferreira@esx.com.br>
2020-07-01 23:36:35 -0300

    app start

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace gpp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
